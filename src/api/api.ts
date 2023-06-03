f4fe0c8706e1eff9d388d1ff8dfc8fbd92b5cc50
saizaax <flamesuspect@gmail.com>
2023-06-04 01:27:33 +0300

    [fix] fixed base-api url to prod

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios"
 
 export const API = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: "https://docstorage.saizaax.xyz/api",
   headers: { "Content-Type": "application/json" }
 })
